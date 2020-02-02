55f51586ada8536dd2b56e18e6e82f01bce13401
kyle Guerrero <kyleguerrero85@gmail.com>
2020-02-02 13:12:35 -0600

    Have a basic site deployed with django

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+# Dummy data for the html
+
+posts = [
+    {
+        "author": "Kyle G",
+        "title": "Blog Post 1",
+        "content": "First post ever",
+        "date_posted": "Feb 2, 2020",
+    },
+    {
+        "author": "Jane Doe",
+        "title": "Blog Post 2",
+        "content": "First post ever",
+        "date_posted": "Feb 2, 2020",
+    }
+]
+
+
+def home(request):
+    context = {"posts": posts}
+    return render(request, "blog/home.html", context)
+
+
+def about(request):
+    return render(request, "blog/about.html", {"title": "About"})
